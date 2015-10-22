f0512cd99faa507183731fa04f4195c75514b279
Etienne Marais <etienne.marais@olx.com>
2015-10-22 21:22:39 +0200

    Build - To fix the build part to properly run and output the image.

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 # Builds the docker images based on your config
+# -t Is the tagname, Then is the actual filename and it gets prefixed with a pipe of the $image
 
 buildDockerImage() {
     outputInYellow "[Building docker image: $*]"
-    docker build -t asgard/$image $BASE_DIR/images/$image | prefix_with "${FUNCNAME[0]}( $image )"
+    docker build -t asgard/$image $BASE_DIR/images/$image | prefixWith "${FUNCNAME[0]}( $image )"
+}
+
+# Prefixes the output with the function name and the current image
+prefixWith () {
+    while read line
+    do
+        echo -n -e "\033[0;34m"
+        printf "%50s: " "$1"
+        echo -n -e "\033[0m"
+        echo $line
+    done
 }
